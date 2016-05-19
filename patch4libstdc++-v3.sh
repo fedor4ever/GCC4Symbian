9ac17233468cbaf6143822c0a5853e6b254e5c8d
Fedor Strizhnev <fedor_qd@mail.ru>
2016-05-19 21:17:35 +0300

    License - Attribution-NonCommercial 4.0 International
    add autopatcher feature for successful libsupc++ build

--- patch4libstdc++-v3.sh
+++ patch4libstdc++-v3.sh
@@ -1,24 +1,42 @@
+#License - Attribution-NonCommercial 4.0 International
+
+# this is tesbed autopatcher for gcc sources
 # patch for the EOF, SEEK_CUR, and SEEK_END integer constants
 # because autoconf can't set them
-var1='as_fn_error "computing EOF failed" "$LINENO" 5'
-var2='$as_echo "computing EOF failed" "$LINENO" >&5'
-sed -i '/'"$var1"'/s/.*''/'"$var2"'/g' configure
-# var1=as_fn_error "computing SEEK_CUR failed" "$LINENO" 5
-# var2=$as_echo "computing SEEK_CUR failed" "$LINENO" >&5
-# sed -i -e 's/'"$var1"'/'"$var2"'/g' /configure
-# var1=as_fn_error "computing SEEK_END failed" "$LINENO" 5
-# var2=$as_echo "computing SEEK_END failed" "$LINENO" >&5
-# sed -i -e 's/'"$var1"'/'"$var2"'/g' /configure
+find='as_fn_error "computing EOF failed" "$LINENO" 5'
+replace='$as_echo "computing EOF failed" "$LINENO" >\&5'
+sed -i -e 's/'"$find"'/'"$replace"'/g' configure
 
-# var1=
-# var2=
-# sed -i -e 's/'"$var1"'/'"$var2"'/g' /configure
-# var1=
-# var2=
-# sed -i -e 's/'"$var1"'/'"$var2"'/g' /configure
-# var1=
-# var2=
-# sed -i -e 's/'"$var1"'/'"$var2"'/g' /configure
-# var1=
-# var2=
-# sed -i -e 's/'"$var1"'/'"$var2"'/g' /configure
+find='as_fn_error "computing SEEK_CUR failed" "$LINENO" 5'
+replace='$as_echo "computing SEEK_CUR failed" "$LINENO" >\&5'
+sed -i -e 's/'"$find"'/'"$replace"'/g' configure
+
+find='as_fn_error "computing SEEK_END failed" "$LINENO" 5'
+replace='$as_echo "computing SEEK_END failed" "$LINENO" >\&5'
+sed -i -e 's/'"$find"'/'"$replace"'/g' configure
+
+patch for the void, int, short and long
+because autoconf can't set them
+find='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (void \*))" "ac_cv_sizeof_void_p"        "$ac_includes_default"'
+replace='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (void \*))" "ac_cv_sizeof_void_p"'
+sed -i -e 's/'"$find"'/'"$replace"'/g' configure
+
+find='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (long))" "ac_cv_sizeof_long"        "$ac_includes_default"'
+replace='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (long))" "ac_cv_sizeof_long"'
+sed -i -e 's/'"$find"'/'"$replace"'/g' configure
+
+find='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (int))" "ac_cv_sizeof_int"        "$ac_includes_default"'
+replace='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (int))" "ac_cv_sizeof_int"'
+sed -i -e 's/'"$find"'/'"$replace"'/g' configure
+
+find='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (short))" "ac_cv_sizeof_short"        "$ac_includes_default"'
+replace='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (short))" "ac_cv_sizeof_short"'
+sed -i -e 's/'"$find"'/'"$replace"'/g' configure
+
+find='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (char))" "ac_cv_sizeof_char"        "$ac_includes_default"'
+replace='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (char))" "ac_cv_sizeof_char"'
+sed -i -e 's/'"$find"'/'"$replace"'/g' configure
+
+# find=''
+# replace=''
+# sed -i -e 's/'"$find"'/'"$replace"'/g' configure
