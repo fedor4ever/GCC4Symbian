3cb50a34ad54e133a104b463c528ed0d6020f4c8
Fiodar Stryzhniou <devuan@toshi>
2022-06-06 12:48:43 +0300

    Remove unneeded code

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -114,47 +114,6 @@ mkdir build-gcc
 
 echo "Building gcc started"
 
-# patch for the EOF, SEEK_CUR, and SEEK_END integer constants
-# because autoconf can't set them
-find='as_fn_error "computing EOF failed" "$LINENO" 5'
-replace='$as_echo "computing EOF failed" "$LINENO" >\&5'
-# echo $replace
-sed -i -e 's/'"$find"'/'"$replace"'/g' $CONFIGURE
-find='as_fn_error "computing SEEK_CUR failed" "$LINENO" 5'
-replace='$as_echo "computing SEEK_CUR failed" "$LINENO" >\&5'
-sed -i -e 's/'"$find"'/'"$replace"'/g' $CONFIGURE
-find='as_fn_error "computing SEEK_END failed" "$LINENO" 5'
-replace='$as_echo "computing SEEK_END failed" "$LINENO" >\&5'
-sed -i -e 's/'"$find"'/'"$replace"'/g' $CONFIGURE
-
-# patch for the void, int, short and long
-# because autoconf can't set them
-find='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (void \*))" "ac_cv_sizeof_void_p"        "$ac_includes_default"'
-replace='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (void \*))" "ac_cv_sizeof_void_p"'
-sed -i -e 's/'"$find"'/'"$replace"'/g' $CONFIGURE
-
-find='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (long))" "ac_cv_sizeof_long"        "$ac_includes_default"'
-replace='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (long))" "ac_cv_sizeof_long"'
-sed -i -e 's/'"$find"'/'"$replace"'/g' $CONFIGURE
-
-find='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (int))" "ac_cv_sizeof_int"        "$ac_includes_default"'
-replace='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (int))" "ac_cv_sizeof_int"'
-sed -i -e 's/'"$find"'/'"$replace"'/g' $CONFIGURE
-
-find='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (short))" "ac_cv_sizeof_short"        "$ac_includes_default"'
-replace='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (short))" "ac_cv_sizeof_short"'
-sed -i -e 's/'"$find"'/'"$replace"'/g' $CONFIGURE
-
-find='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (char))" "ac_cv_sizeof_char"        "$ac_includes_default"'
-replace='if ac_fn_c_compute_int "$LINENO" "(long int) (sizeof (char))" "ac_cv_sizeof_char"'
-sed -i -e 's/'"$find"'/'"$replace"'/g' $CONFIGURE
-
-# find=''
-# replace=''
-# sed -i -e 's/'"$find"'/'"$replace"'/g' $CONFIGURE
-
-
-
 touch build-gcc-started
 cd build-gcc
 ../$GCCC/./configure  --target=$TARGET --prefix=$PREFIX $ICONV --without-headers \
